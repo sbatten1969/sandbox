9dafe2fb582998341db49715a808e1666c5b2fd3
SBatten <sbatten1969@users.noreply.github.com>
2017-01-28 11:31:07 -0500

    Create Main.java

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+// Package naming is not allowed in online IDEs (or IS it?)
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
